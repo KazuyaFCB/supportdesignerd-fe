6178d8ae08a1a0550416a21b0f6292ac49df25e6
KazuyaFCB <hoangvinh175@gmail.com>
2021-07-01 19:55:58 +0700

    fix sign up page ui

--- src/components/Guide/index.js
+++ src/components/Guide/index.js
@@ -27,10 +27,10 @@ function Guide({ inJsonPage }) {
           {inJsonPage && (
             <React.Fragment>
               <li className="help-items">
-                <strong>Element JSON</strong> là phần json data thể hiện cấu
+                <strong>Element JSON</strong> là phần dữ liệu JSON thể hiện cấu
                 trúc của các <strong> thực thể/mối kết hợp/thuộc tính</strong>{" "}
-                trên diagram, có thể chỉnh sửa để thay đổi nội dung của diagram.
-                Bao gồm kích thước, thể loại, id, nội dung, vị trí,...
+                trên mô hình, có thể chỉnh sửa để thay đổi nội dung của mô hình.
+                Bao gồm <strong>{" "} id, tọa độ, thể loại, nội dung, kích cỡ</strong>.
               </li>
               <li className="help-items">
                 <strong>Cấu trúc của elementJSON: </strong>
@@ -49,18 +49,17 @@ function Guide({ inJsonPage }) {
                 {" "}<strong>thực thể/mối kết hợp/thuộc tính</strong>
                 {" .Còn "}<strong>id, x, y, type, paragraph, width, height</strong>, lần lượt là 
                   <strong>{" "}id, tọa độ, thể loại, nội dung, kích cỡ</strong> của <strong>thực thể/mối kết hợp/thuộc tính{" "}</strong> 
-                  trên diagram. Đồng thời <strong>id</strong> phải bắt đầu từ 1.
+                  trên mô hình. Đồng thời <strong>id</strong> phải bắt đầu từ 1.
                 Ngoài ra, <strong>type</strong>
                 {` phải là 1 trong các chuỗi sau: `}
                 <strong>"Entity", "WeakEntity", "Relationship", "IdentifyingRelationship", 
                 "Attribute", "Key", "Multivalued", "Derived", 
-                "PartialKeyAttribute", "AssociativeEntity"</strong>
+                "PartialKeyAttribute", "AssociativeEntity"</strong>.
               </li>
               <li className="help-items">
-                <strong>Link JSON</strong> là phần json data thể hiện của các
-                <strong> đường nối </strong> trên diagram, có thể chỉnh sửa để
-                thay đổi nội dung của diagram. Bao gồm thể loại, id, thực thể 1,
-                thực thể 2,...
+                <strong>Link JSON</strong> là phần dữ liệu JSON thể hiện của các
+                <strong> đường nối </strong> trên mô hình, có thể chỉnh sửa để
+                thay đổi nội dung của mô hình. Bao gồm <strong>id, thể loại, nội dung, id đối tượng tại 2 đầu mút</strong>.
               </li>
               <li className="help-items">
                 <strong>Cấu trúc của linkJSON: </strong>
@@ -79,24 +78,24 @@ function Guide({ inJsonPage }) {
                 {" "}<strong>đường nối</strong>
                 {" .Còn "}<strong>id, type, paragraph, sourceId, targetId</strong> lần lượt là 
                   <strong>{" "}id, thể loại, nội dung, id thực thể/mối kết hợp/thuộc tính tại 2 đầu mút
-                  của đường nối trên diagram</strong>. 
+                  của đường nối trên mô hình</strong>. 
                   Đồng thời <strong>id</strong> phải bắt đầu từ 1.
                   Ngoài ra, <strong>type</strong>
                   {` phải là 1 trong các chuỗi sau: `}
-                  <strong>"PartialParticipation", "TotalParticipation", "Optional"</strong>
+                  <strong>"PartialParticipation", "TotalParticipation", "Optional"</strong>.
               </li>
             </React.Fragment>
           )}
           <li className="help-items">
-            Để vẽ <strong>thực thể/mối kết hợp/thuộc tính</strong> lên diagram,
+            Để vẽ <strong>thực thể/mối kết hợp/thuộc tính</strong> lên mô hình,
             click vào các{" "}
             <strong>ký hiệu hình chữ nhật/hình thoi/hình elip </strong>
             trên thanh công cụ ở dưới, sẽ có 1 thực thể/mối kết hợp/thuộc tính
-            được tạo ở góc trái trên của diagram.
+            được tạo ở góc trái trên của mô hình.
           </li>
           <li className="help-items">
             Để vẽ <strong>đường nối</strong> giữa 2{" "}
-            <strong>thực thể/mối kết hợp/thuộc tính</strong> lên diagram, click
+            <strong>thực thể/mối kết hợp/thuộc tính</strong> lên mô hình, click
             vào các <strong>ký hiệu đường nối</strong> trên thanh công cụ. Rồi
             click vào <strong>thực thể/mối kết hợp/thuộc tính THỨ 1 </strong>
             và tiếp đó click vào{" "}
@@ -105,19 +104,19 @@ function Guide({ inJsonPage }) {
           <li className="help-items">
             Khi muốn bỏ chọn một <strong>ký hiệu đường nối</strong> trên thanh
             công cụ, <strong>click chuột vào khoảng trống</strong> trên vùng
-            diagram.
+            mô hình.
           </li>
           <li className="help-items">
             Khi chỉnh sửa nội dung{" "}
             <strong>thực thể/mối kết hợp/thuộc tính/đường nối </strong>
             thì click đúp chuột vào chúng, rồi nhập nội dung cần sửa, sau đó
-            click khoảng trống trên diagram để diagram tự động nạp lại nội dung
+            click khoảng trống trên mô hình để mô hình tự động nạp lại nội dung
             đã nhập.
           </li>
           <li className="help-items">
             Khi muốn xóa{" "}
             <strong>thực thể/mối kết hợp/thuộc tính/đường nối</strong> trên
-            diagram thì đưa chuột vào hình vẽ cần xóa, sẽ có{" "}
+            mô hình thì đưa chuột vào hình vẽ cần xóa, sẽ có{" "}
             <strong>icon "X"</strong> hiện lên, click vào{" "}
             <strong>icon "X"</strong> để xoá hình.
           </li>
@@ -125,7 +124,7 @@ function Guide({ inJsonPage }) {
             Trong trường hợp có cảnh báo lỗi chưa điền bảng số, sau đó{" "}
             <strong>đã điền bảng số</strong> cho các liên kết nhưng cảnh báo lỗi
             vẫn xuất hiện, <strong>vui lòng refresh lại trang (ấn F5)</strong>{" "}
-            để nạp lại diagram và điền lại bảng số.
+            để nạp lại mô hình và điền lại bảng số.
           </li>
           <li className="help-items">
             Để xem nội dung <strong>lỗi logic (nếu có)</strong> của{" "}
